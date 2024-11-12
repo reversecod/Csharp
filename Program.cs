67da5be934f10ac48d8447d4d46881d419728689
reversecod <otavioresendeoficial@gmail.com>
2024-11-12 09:19:31 -0300

    Adição de códigos fontes ao diretório Csharp

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace GettingInput 
+{
+    class Program { 
+        static void Main()
+        {
+            Console.WriteLine("Quantos anos você tem?:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Quantos anos tem seu amigo(a)?:");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            int soma = num1 + num2;
+            Console.WriteLine($"A soma das suas idades é: {soma}");
+        }
+    }
+}
